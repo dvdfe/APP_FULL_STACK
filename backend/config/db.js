d7e38f9769be80358acff8b5d0f4f20d20a0508b
DVDFE <davidfe.pro@gmail.com>
2023-08-10 18:18:24 +0200

    creation de model mongoose post

---
+++ backend/config/db.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+
+mongoose
+  .connect(process.env.DB_ADMIN, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,47 @@
-const express = require("express");
-const port = 5000;
+const http = require('http');
+const app = require('./config/db');
 
-const app = express();
+const normalizePort = val => {
+  const port = parseInt(val, 10);
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use("/post", require("./routes/post.routes"));
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
-app.listen(port, () => console.log("Le server a démarré au port: " + port));
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+server.listen(port);
