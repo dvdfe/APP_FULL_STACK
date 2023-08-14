80e4eaffe55152e63b2ed1c11b643afaa9b405e7
DVDFE <davidfe.pro@gmail.com>
2023-08-14 15:08:10 +0200

    Ajout de la logique metier et des routes likes et dislikes

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -68,3 +68,27 @@ module.exports.deletePost = async (req, res) => {
     res.status(500).json({ message: "Erreur lors de la suppression du post" });
   }
 };
+
+module.exports.likePost = async (req, res) => {
+  try {
+    await PostModel.findByIdAndUpdate(
+      req.params.id,
+      { $addToSet: { likers: req.body.userId } },
+      { new: true }
+    ).then((data) => res.status(200).send(data));
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+module.exports.dislikePost = async (req, res) => {
+  try {
+    await PostModel.findByIdAndUpdate(
+      req.params.id,
+      { $pull: { likers: req.body.userId } },
+      { new: true }
+    ).then((data) => res.status(200).send(data));
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,20 +1,12 @@
 const express = require("express");
-const { createPost, getPosts, editPost, deletePost } = require("../controllers/post.controller");
+const { createPost, getPosts, editPost, deletePost, likePost, dislikePost } = require("../controllers/post.controller");
 const router = express.Router();
 
 router.get("/", getPosts);
-
 router.post("/", createPost);
-
 router.put("/:id", editPost);
-
 router.delete("/:id", deletePost);
+router.patch("/like-post/:id", likePost);
+router.patch("/dislike-post/:id", dislikePost);
 
-router.patch("/like-post/:id", (req, res) => {
-  res.json({ message: "Post liké: id" + req.params.id });
-});
-
-router.patch("/dislike-post/:id", (req, res) => {
-  res.json({ message: "Post disliké: id" + req.params.id });
-});
 module.exports = router;
