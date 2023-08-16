310215042c538dc2b7c97d35f3d6954867572a72
DVDFE <davidfe.pro@gmail.com>
2023-08-16 15:07:02 +0200

    ajout des derniers components et fonctions CRUD + ajout du style

---
+++ frontend/src/components/DeletePost.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import React from 'react';
+
+const DeletePost = ({postId}) => {
+
+   const handleDelete = ()=>{
+       axios.delete("http://localhost:3000/post/" + postId)
+   }
+    return (
+        <span id="delete-btn" onClick={handleDelete}>&#10010;</span>
+    );
+};
+
+export default DeletePost;
--- frontend/src/components/LikePost.js
+++ frontend/src/components/LikePost.js
@@ -1,15 +1,47 @@
 import axios from "axios";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const LikePost = ({ post, userId }) => {
+  const [userLiked, setUserLiked] = useState(false);
 
-    const likePost = () => {
-        axios.patch("http://localhost:3000/post/like-post/" + post._id, {userId})
+  useEffect(() => {
+    if (post.likers) {
+      if (post.likers.includes(userId)) {
+        setUserLiked(true);
+      } else {
+        setUserLiked(false);
+      }
     }
+  }, [post.likers, userId]);
+
+  const likePost = () => {
+    axios.patch(`http://localhost:3000/post/like-post/${post._id}`, { userId })
+      .then(() => setUserLiked(true))
+      .catch((error) => {
+        console.error("Error liking post:", error);
+      });
+  };
+
+  const dislikePost = () => {
+    axios.patch(`http://localhost:3000/post/dislike-post/${post._id}`, { userId })
+      .then(() => setUserLiked(false))
+      .catch((error) => {
+        console.error("Error disliking post:", error);
+      });
+  };
+
   return (
     <div className="like-icon">
       <p>{post.likers ? post.likers.length : 0}</p>
-      <span id="dislike-btn" onClick={likePost()}>&#9829;</span>
+      {userLiked ? (
+        <span id="like-btn" onClick={dislikePost}>
+          &#9829;
+        </span>
+      ) : (
+        <span id="dislike-btn" onClick={likePost}>
+          &#9829;
+        </span>
+      )}
     </div>
   );
 };
